83b559f755b61bae187b2ef09cef19f74320e6a1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-08-21 14:03:48 +0200

    Refactor the routes api

--- projects/angular-ui-generator/src/lib/routes/api/aug-routes.ts
+++ projects/angular-ui-generator/src/lib/routes/api/aug-routes.ts
@@ -1,14 +1,9 @@
-export const augRoutes = () => {
-  //
-};
+import { Routes } from '@angular/router';
+import { AugViewGenerator } from '../../generator';
+import { routesDomainBuilder } from '../builder/routes-domain.builder';
+import { angularRoutesBuilder } from '../builder/angular-routes.builder';
 
-// import { Routes } from '@angular/router';
-// import { routesDomainBuilder } from '../builder/routes-domain.builder';
-// import { angularRoutesBuilder } from '../builder/routes.builder';
-//
-// // todo: Change the any type to the correct type
-// // eslint-disable-next-line @typescript-eslint/no-explicit-any
-// export const augRoutes = (views: any[]): Routes => {
-//   const routesDomains = routesDomainBuilder(views);
-//   return angularRoutesBuilder(routesDomains);
-// };
+export const augRoutes = (views: AugViewGenerator[]): Routes => {
+  const routesDomains = routesDomainBuilder(views);
+  return angularRoutesBuilder(routesDomains);
+};
